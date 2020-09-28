9ff59a2a5ab2d46c1607921007820b7f454ca689
Grant Marvin <rogueg@gmail.com>
2020-09-28 15:49:27 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,8 +4,7 @@ import PackageDescription
 let package = Package(
     name: "websocket-kit",
     platforms: [
-       .macOS(.v10_14),
-       .macOS(.v10_15),
+       .macOS(.v10_14)
     ],
     products: [
         .library(name: "WebSocketKit", targets: ["WebSocketKit"]),
